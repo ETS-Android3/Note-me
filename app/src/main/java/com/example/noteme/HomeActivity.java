ffecfc5599b81f44fbbd8448a523274b378c122c
Prometheus <emanuel.frias17@tectijuana.edu.mx>
2021-11-08 13:35:07 -0800

    Intents agregados para navegacion de prueba

--- app/src/main/java/com/example/noteme/HomeActivity.java
+++ app/src/main/java/com/example/noteme/HomeActivity.java
@@ -2,13 +2,36 @@ package com.example.noteme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
+    ImageButton btnImg1, btnImg2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnImg1 = findViewById(R.id.btnImg1);
+        btnImg2 = findViewById(R.id.btnImg2);
+
+        btnImg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeActivity.this, TemaActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnImg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeActivity.this, TemaActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/noteme/TemaActivity.java
+++ app/src/main/java/com/example/noteme/TemaActivity.java
@@ -2,13 +2,51 @@ package com.example.noteme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TemaActivity extends AppCompatActivity {
+    TextView txtBtn1, txtBtn2, txtBtn3,txtBtn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tema);
+
+        txtBtn1 = findViewById(R.id.txtBtn1);
+        txtBtn2 = findViewById(R.id.txtBtn2);
+        txtBtn3 = findViewById(R.id.txtBtn3);
+        txtBtn4 = findViewById(R.id.txtBtn4);
+
+        txtBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TemaActivity.this, InfoTemaActivity.class);
+                startActivity(i);
+            }
+        });
+        txtBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TemaActivity.this, InfoTemaActivity.class);
+                startActivity(i);
+            }
+        });
+        txtBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TemaActivity.this, InfoTemaActivity.class);
+                startActivity(i);
+            }
+        });
+        txtBtn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TemaActivity.this, InfoTemaActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
